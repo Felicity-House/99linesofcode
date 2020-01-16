92c59ed00f9b1b81e6d22d41946fe16c785a5416
felicity139 <felicity.house@yahoo.com>
2020-01-16 10:19:46 -0600

    99final

---
+++ 99.js
@@ -0,0 +1,60 @@
+//for (i = 0; i < friends.length; i++) {
+//  for (let j = 99; j > 0; j--) {
+//    if ( j > 1) {
+//      console.log(j + ' lines of code in the file')
+//}
+//else if (j == 1) {
+//  console.log(j + ' line of code in the file')
+//}
+//}
+//}
+
+//if(i == 0){
+//console.log("done")
+
+let friends = ['smita', 'paige', 'shaina', 'deja', 'cameron'];
+
+for (let i = 0; i < friends.length; i++) {
+  console.log(friends[i] + ':')
+  for (var j = 100; j > 0; j--) {
+    if (j === 1) {
+      console.log(j + " line of code on the wall " + j + " line of code " + friends[i] + " strikes one out clears it all out " + (j - 1) + " lines of code on the wall ")
+    }
+    else if (j != 1) {
+      console.log(j + " lines of code on the wall " + j + " lines of code " + friends[i] + " strikes one out clears it all out " + (j - 1) + " lines of code on the wall ")
+    }
+    
+  }
+}
+
+// console.log(friends[0])
+
+// for (var i = 100; i > 0; i--) {
+//   console.log(i + " lines of code on the wall " + i + " lines of code smita strikes one out clears it all out " + (i - 1) + " lines of code on the wall ")
+// }
+
+// console.log(friends[1])
+
+// for (var i = 100; i > 0; i--) {
+//   console.log(i + " lines of code on the wall " + i + " lines of code paige strikes one out clears it all out " + (i - 1) + " lines of code on the wall ")
+// }
+
+// console.log(friends[2])
+
+// for (var i = 100; i > 0; i--) {
+//   console.log(i + " lines of code on the wall " + i + " lines of code shaina strikes one out clears it all out " + (i - 1) + " lines of code on the wall ")
+// }
+
+// console.log(friends[3])
+
+// for (var i = 100; i > 0; i--) {
+//   console.log(i + " lines of code on the wall " + i + " lines of code deja strikes one out clears it all out " + (i - 1) + " lines of code on the wall ")
+// }
+
+// console.log(friends[4])
+
+// for (var i = 100; i > 0; i--) {
+//   console.log(i + " lines of code on the wall " + i + " lines of code cameron strikes one out clears it all out " + (i - 1) + " lines of code on the wall ")
+// }
+
+
